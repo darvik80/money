514534929796ada7f81ae27ac6f8cdaa01309bd0
Ivan Kishchenko <darvik@crearts.xyz>
2018-05-30 19:35:48 +0700

    security issue

--- src/main/java/xyz/crearts/money/configuration/WebSecurityConfig.java
+++ src/main/java/xyz/crearts/money/configuration/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package xyz.crearts.money.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
